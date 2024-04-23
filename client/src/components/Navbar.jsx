e50aa1ebea17f797080f23e3e42942e8e8b6b955
Jospin6 <jospinndagano1@gmail.com>
2024-04-23 14:08:16 +0200

    Add the app title to the navbar

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,6 @@
+
+export const Navbar = () => {
+    return <div className="w-full h-[50px] border-b-[1px] border-gray-300 fixed z-10">
+        <div className="pl-4 leading-[50px] semi-bold">petsPics</div>
+    </div>
+}
--- client/src/containers/Home.jsx
+++ client/src/containers/Home.jsx
@@ -2,6 +2,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { getUsers, currentUser } from '../helpers/userHelper'
 import { fetchUsers } from '../slices/user/userApi'
 import { useEffect } from 'react'
+import { Navbar } from '../components/Navbar';
 
 export const Home = () => {
     const users = useSelector(getUsers)
@@ -13,12 +14,15 @@ export const Home = () => {
     }, [dispatch])
 
     return <div>
-        <div>
-            user name: {activeUser.userName}
-        </div>
-        {   users.map(user => (
+        <Navbar />
+        <div className='pt-[50px]'>
+            <div>
+                user name: {activeUser.userName}
+            </div>
+            {users.map(user => (
                 <div className='text-red-500' key={user.id}> {user.userName} </div>
             ))
-        }
+            }
+        </div>
     </div>
 }
