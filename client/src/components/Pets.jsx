07f1b43a5f713c4eceacdfbf31ae027485f20e31
Jospin6 <jospinndagano1@gmail.com>
2024-04-24 11:37:45 +0200

    Add routes to manage components in the home page

---
+++ client/src/components/Pets.jsx
@@ -0,0 +1,8 @@
+import { PostItem } from "../containers/PostItem"
+
+export const Pets = () => {
+    return <div>
+        <PostItem/>
+        <PostItem/>
+    </div>
+}
--- client/src/containers/Home.jsx
+++ client/src/containers/Home.jsx
@@ -1,33 +1,17 @@
-import { useDispatch, useSelector } from 'react-redux'
-import { getUsers, currentUser } from '../helpers/userHelper'
-import { fetchUsers } from '../slices/user/userApi'
-import { useEffect } from 'react'
 import { Navbar } from '../components/Navbar';
-import { PostItem } from './PostItem';
+import { Route, Routes } from 'react-router-dom';
+import { Pets } from '../components/Pets';
 
 export const Home = () => {
-    const users = useSelector(getUsers)
-    const activeUser = useSelector(currentUser)
-    const dispatch = useDispatch()
-
-    useEffect(() => {
-        dispatch(fetchUsers())
-    }, [dispatch])
 
     return <div>
         <Navbar />
         <div className='pt-[50px] w-full bg-gray-100 pb-4'>
             <div className='w-[40%] m-auto min-h-[400px] h-auto'>
-                <PostItem/>
-                <PostItem/>
+                <Routes>
+                    <Route path='/' element={<Pets/>} />
+                </Routes>
             </div>
-            {/* <div>
-                user name: {activeUser.userName}
-            </div>
-            {users.map(user => (
-                <div className='text-red-500' key={user.id}> {user.userName} </div>
-            ))
-            } */}
         </div>
     </div>
 }
