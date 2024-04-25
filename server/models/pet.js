5b64042fd2c45ad4a1cecfd471095bd2cd9c8b26
Jospin6 <jospinndagano1@gmail.com>
2024-04-25 14:23:48 +0200

    Create the Pet model with his migration using sequelize-cli

---
+++ server/models/pet.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Pet extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Pet.init({
+    petName: DataTypes.STRING,
+    species: DataTypes.STRING,
+    breed: DataTypes.STRING,
+    age: DataTypes.DATE,
+    user_id: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Pet',
+  });
+  return Pet;
+};
