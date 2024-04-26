d3dd1c17ee7482accb2c0c2a9a6904fa83ced933
Jospin6 <jospinndagano1@gmail.com>
2024-04-26 14:30:12 +0200

    Create the show comments router

--- server/routes/Comments.js
+++ server/routes/Comments.js
@@ -2,9 +2,11 @@ const express = require("express")
 const router = new express.Router()
 const {validateToken} = require("../middlewares/authMiddleware")
 const {
-    create
+    create,
+    show
 } = require("../controllers/commentController")
 
+router.get("/comments/post_id", validateToken, show)
 router.post("/comments", validateToken, create)
 
 module.exports = router
