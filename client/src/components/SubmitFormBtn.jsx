846d5158b7c3a07534408cbee6731805dd14ff35
Jospin6 <jospinndagano1@gmail.com>
2024-04-26 15:48:57 +0200

    Refactor code for submit form btn component

--- client/src/components/SubmitFormBtn.jsx
+++ client/src/components/SubmitFormBtn.jsx
@@ -1,6 +1,9 @@
 
 export const SubmitFormBtn = ({text, className}) => {
-    return <button type="submit" className={`py-[5px] bg-blue-500 mt-4 rounded-md text-white ${className}`}>
+    return <button 
+        type="submit" 
+        className={`py-[5px] bg-blue-500 mt-4 
+        rounded-md text-white ${className}`}>
         { text }
     </button>
 }
