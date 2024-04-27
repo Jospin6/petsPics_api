936973101b2324680f69ef6cb4cb0b4c77662091
Jospin6 <jospinndagano1@gmail.com>
2024-04-27 14:04:22 +0200

    Display post item in the comment component

--- client/src/components/Comments.jsx
+++ client/src/components/Comments.jsx
@@ -1,6 +1,13 @@
+import { PostItem } from "./PostItem"
 
 export const Comments = () => {
-    return <div>
-        This is the comments part
+    return <div className="mt-2">
+        <PostItem showCommentIcon="hidden"/>
+        <div className="h-[40px] rounded-lg bg-white mt-2">
+            commentfield
+        </div>
+        <div className="bg-white rounded-lg mt-2 h-[200px] p-2">
+            comments
+        </div>
     </div>
 }
