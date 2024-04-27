ba1c30281ea62f55f0b64694f8f089b1b5c6dd0d
Jospin6 <jospinndagano1@gmail.com>
2024-04-27 12:38:29 +0200

    Add font weight to the titles in createPet component

--- client/src/components/CreatePet.jsx
+++ client/src/components/CreatePet.jsx
@@ -7,12 +7,12 @@ export const CreatePet = () => {
 
     return <div className="text-black">
         <div className="w-full h-[320px] rounded-lg bg-white mt-4 p-2">
-            <div className="text-lg arial">Add new pet</div>
+            <div className="text-lg arial semi-bold">Add new pet</div>
             <NewPetForm/>
         </div>
         <div className="w-full h-[250px] mt-4">
             <div className="p-2 arial flex justify-between rounded-lg bg-white">
-                <span className="text-lg">Your pets</span>
+                <span className="text-lg semi-bold">Your pets</span>
                 <Link to="/user/post">
                     <Button text="Post pet" className="bg-green-500" />
                 </Link>
