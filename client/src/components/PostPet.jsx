1380648c42a1526bfd6f269dbb5be5d2a5ba4871
Jospin6 <jospinndagano1@gmail.com>
2024-04-27 11:54:08 +0200

    Rename PostPetModal to PostPet

--- client/src/components/PostPet.jsx
+++ client/src/components/PostPet.jsx
@@ -1,5 +1,5 @@
 
-export const PostPetModal = () => {
+export const PostPet = () => {
     return <div className="z-10 fixed top-0 right-0 w-full
      h-[100vh] text-white blackBgWithOpacity flex items-center justify-center">
         <div className="w-[500px] h-[400px] rounded-lg bg-white">
