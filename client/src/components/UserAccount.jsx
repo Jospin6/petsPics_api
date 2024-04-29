43ff82d517c3044770af63fdb83795465224885c
Jospin6 <jospinndagano1@gmail.com>
2024-04-29 12:45:27 +0200

    Create blocs of user name and password modifications form

--- client/src/components/UserAccount.jsx
+++ client/src/components/UserAccount.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 import { MiniPetCard } from "./MiniPetCard"
 import { PostItem } from "./PostItem"
 
@@ -5,7 +6,9 @@ export const UserAccount = () => {
     return <div>
         <div className="h-[50px] flex items-center bg-white px-2 justify-between">
             <span className="text-[20px] semi-bold">user name</span> 
-            <span className="text-blue-600 text-[14px]">modify</span>
+            <Link to="/user/infos">
+                <span className="text-blue-600 text-[14px]">modify</span>
+            </Link>
         </div>
         <div className="flex">
             <MiniPetCard/>
--- client/src/components/UserInfo.jsx
+++ client/src/components/UserInfo.jsx
@@ -1,6 +1,11 @@
 
 export const UserInfo = () => {
     return <div>
-        <div>user infos</div>
+        <div className="bg-white rounded-lg min-h-[100px] mt-2 p-2">
+            form for userName
+        </div>
+        <div className="bg-white rounded-lg min-h-[150px] mt-2 p-2">
+            form form mdp
+        </div>
     </div>
 }
