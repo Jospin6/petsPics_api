c19ab44827c56428484e1960c2a21c4f4622e6f3
Jospin6 <jospinndagano1@gmail.com>
2024-04-29 09:22:27 +0200

    Delete the get user's likes function that's unnecessary

--- server/routes/Likes.js
+++ server/routes/Likes.js
@@ -1,9 +1,8 @@
 const express = require("express")
-const { create, getUserLikes } = require("../controllers/likeController")
+const { create } = require("../controllers/likeController")
 const { validateToken } = require("../middlewares/authMiddleware")
 const router = new express.Router()
 
 router.post("/like", validateToken , create)
-router.get("/like/:id", validateToken , getUserLikes)
 
 module.exports = router
