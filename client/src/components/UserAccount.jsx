e39c34c25cd0b341905794bf1119f3221f6508cc
Jospin6 <jospinndagano1@gmail.com>
2024-04-29 10:14:05 +0200

    Design the user account component

--- client/src/components/UserAccount.jsx
+++ client/src/components/UserAccount.jsx
@@ -1,6 +1,17 @@
+import { MiniPetCard } from "./MiniPetCard"
+import { PostItem } from "./PostItem"
 
 export const UserAccount = () => {
     return <div>
-        your account
+        <div className="h-[50px] flex items-center bg-white px-2 justify-between">
+            <span className="text-[20px] semi-bold">user name</span> 
+            <span className="text-blue-600 text-[14px]">modify</span>
+        </div>
+        <div className="flex">
+            <MiniPetCard/>
+        </div>
+        <div>
+            <PostItem/>
+        </div>
     </div>
 }
