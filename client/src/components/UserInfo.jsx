895976f73240eac547b65abb50f6448aa532e420
Jospin6 <jospinndagano1@gmail.com>
2024-04-30 09:39:42 +0200

    Create form to modify user name

--- client/src/components/UserInfo.jsx
+++ client/src/components/UserInfo.jsx
@@ -1,11 +1,32 @@
+import { Formik, Form } from 'formik'
+import { Input } from './Input'
+import { SubmitFormBtn } from './SubmitFormBtn'
 
 export const UserInfo = () => {
     return <div>
         <div className="bg-white rounded-lg min-h-[100px] mt-2 p-2">
-            form for userName
+            <div className="semi-bold text-[18px]">
+                Change your name
+            </div>
+            <div>
+                <Formik>
+                    <Form>
+                        <Input
+                            labelText="User name"
+                            id="userName"
+                            type="text"
+                            fieldName="userName"
+                            placeholder="Enter your name"
+                        />
+                        <div className='flex justify-end'>
+                            <SubmitFormBtn text="Modify" className="w-[150px]" />
+                        </div>
+                    </Form>
+                </Formik>
+            </div>
         </div>
         <div className="bg-white rounded-lg min-h-[150px] mt-2 p-2">
-            form form mdp
+            
         </div>
     </div>
 }
