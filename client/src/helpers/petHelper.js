d054f954dc3de4eaed5bd96b79bc35f7b243f204
Jospin6 <jospinndagano1@gmail.com>
2024-05-05 17:24:34 +0200

    Create the function getToday

--- client/src/helpers/petHelper.js
+++ client/src/helpers/petHelper.js
@@ -1,4 +1,25 @@
 
+const handleDate = (dateTme) => {
+    let devidDate = dateTme.split("T")
+    const date = devidDate[0]
+    const time = devidDate[1].slice(0,5)
+    let fullDate = ''
+    if (date === gettoday()) {
+        fullDate = 'today'.concat(". at ").concat(time)
+    }
+    return fullDate
+}
+
+const gettoday = () => {
+    const today = new Date()
+    return today.getFullYear
+        .concat("-")
+        .concat(today.getMonth + 1)
+        .concat("-")
+        .concat(today.getDate())
+}
+
+
 const getPets = state => state.pet.pets
 const getPosts = state => state.post.posts
 
