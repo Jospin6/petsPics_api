5f79e9913e4033efc108936311477eb01ea739c6
Jospin6 <jospinndagano1@gmail.com>
2024-05-07 09:58:03 +0200

    Add association between post and like models

--- server/models/post.js
+++ server/models/post.js
@@ -10,7 +10,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Post.hasMany(models.Like, {
+        foreignKey: 'post_id',
+        onDelete: 'CASCADE'
+      })
     }
   }
   Post.init({
