fbf5f5a3ee01d8dbc0466326bbcdfcb70d5e728b
Jospin6 <jospinndagano1@gmail.com>
2024-05-08 11:50:11 +0200

    Refactor code in comments.jsx component

--- client/src/components/Comments.jsx
+++ client/src/components/Comments.jsx
@@ -17,7 +17,7 @@ export const Comments = () => {
 
     const postComment = () => {
         if (comment != "") {
-            dispatch(createComment({post_id: id, content: comment}))
+            dispatch(createComment({ post_id: id, content: comment }))
             setComment("")
         }
     }
@@ -29,10 +29,12 @@ export const Comments = () => {
     return <div className="mt-2">
         {/* <PostItem showCommentIcon="hidden"/> */}
         <div className="h-[40px] rounded-lg bg-white mt-2 flex justify-between items-center">
-            <input type="text" value={comment} onChange={handleComment} className="w-[94%] h-[35px] border-[1px] border-gray-400 
-            rounded-lg pl-2 outline-none text-black" placeholder="Enter your comment" />
+            <input type="text" value={comment} onChange={handleComment}
+                className="w-[94%] h-[35px] border-[1px] border-gray-400 
+                rounded-lg pl-2 outline-none text-black" 
+                placeholder="Enter your comment" />
             <button onClick={postComment}>
-                <Image src={paper_plane} alt="send" style="w-[25px] h-[25px] " />
+                <Image src={paper_plane} alt="send comment" style="w-[25px] h-[25px] " />
             </button>
         </div>
         <div className="bg-white rounded-lg mt-2 h-auto p-2">
