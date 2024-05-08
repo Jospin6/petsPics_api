a471d28fc6843abe4c7c1a51245c059013d87bff
Jospin6 <jospinndagano1@gmail.com>
2024-05-08 09:11:32 +0200

    Change post_id and user_id to PostId and UserId in comments table

--- client/src/components/Comments.jsx
+++ client/src/components/Comments.jsx
@@ -5,7 +5,7 @@ import { CommentItem } from "./CommentItem"
 
 export const Comments = () => {
     return <div className="mt-2">
-        <PostItem showCommentIcon="hidden"/>
+        {/* <PostItem showCommentIcon="hidden"/> */}
         <div className="h-[40px] rounded-lg bg-white mt-2 flex justify-between items-center">
             <input type="text" className="w-[94%] h-[35px] border-[1px] border-gray-400 
             rounded-lg pl-2 outline-none text-black" placeholder="Enter your comment"/>
