0dc709b5c1ef07392d2606d71fe0f129b44604ea
Jospin6 <jospinndagano1@gmail.com>
2024-05-08 10:02:06 +0200

    Add a function to cretae new comment

--- client/src/slices/comments/commentApi.js
+++ client/src/slices/comments/commentApi.js
@@ -7,3 +7,11 @@ export const fetchPostComments = createAsyncThunk("comments/fetchPostComments", 
         .then(response => response.data)
         .catch(error => error.message)
 })
+
+export const createComment = createAsyncThunk("comments/createComment", async (data) => {
+    return await axios.post("http://localhost:3001/comments", data, {
+        headers: {
+            accessToken: localStorage.getItem("accessToken")
+        }
+    })
+})
