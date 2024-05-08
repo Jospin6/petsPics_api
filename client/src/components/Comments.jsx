f470967b02e3f178d8686cf209962a9a4f036845
Jospin6 <jospinndagano1@gmail.com>
2024-05-08 11:35:50 +0200

    Create new comments

--- client/src/components/Comments.jsx
+++ client/src/components/Comments.jsx
@@ -4,14 +4,23 @@ import paper_plane from '../assets/images/paper_plane.png'
 import { CommentItem } from "./CommentItem"
 import { useParams } from "react-router-dom"
 import { useDispatch, useSelector } from "react-redux"
-import { fetchPostComments } from '../slices/comments/commentApi'
-import { useEffect } from "react"
+import { fetchPostComments, createComment } from '../slices/comments/commentApi'
+import { useEffect, useState } from "react"
 import { getComments } from '../helpers/commentHelper'
 
 export const Comments = () => {
     const { id } = useParams()
     const comments = useSelector(getComments)
     const dispatch = useDispatch()
+    const [comment, setComment] = useState("")
+    const handleComment = (e) => setComment(e.target.value)
+
+    const postComment = () => {
+        if (comment != "") {
+            dispatch(createComment({post_id: id, content: comment}))
+            setComment("")
+        }
+    }
 
     useEffect(() => {
         dispatch(fetchPostComments(id))
@@ -20,13 +29,15 @@ export const Comments = () => {
     return <div className="mt-2">
         {/* <PostItem showCommentIcon="hidden"/> */}
         <div className="h-[40px] rounded-lg bg-white mt-2 flex justify-between items-center">
-            <input type="text" className="w-[94%] h-[35px] border-[1px] border-gray-400 
-            rounded-lg pl-2 outline-none text-black" placeholder="Enter your comment"/>
-            <Image src={paper_plane} alt="send" style="w-[25px] h-[25px] "/>
+            <input type="text" value={comment} onChange={handleComment} className="w-[94%] h-[35px] border-[1px] border-gray-400 
+            rounded-lg pl-2 outline-none text-black" placeholder="Enter your comment" />
+            <button onClick={postComment}>
+                <Image src={paper_plane} alt="send" style="w-[25px] h-[25px] " />
+            </button>
         </div>
         <div className="bg-white rounded-lg mt-2 h-auto p-2">
             {
-                comments.map(comment => (<CommentItem comment={comment} key={comment.id}/>))
+                comments.map(comment => (<CommentItem comment={comment} key={comment.id} />))
             }
         </div>
     </div>
