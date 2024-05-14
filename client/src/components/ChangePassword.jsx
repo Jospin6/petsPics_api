1e8ee56fd7010509aa4a7e673cb847f6f3d7a1f3
Jospin6 <jospinndagano1@gmail.com>
2024-05-14 12:04:38 +0200

    Connect the api with the form to dispatch update password action

--- client/src/components/ChangePassword.jsx
+++ client/src/components/ChangePassword.jsx
@@ -1,22 +1,44 @@
 import { Formik, Form } from 'formik'
 import { Input } from './Input'
 import { SubmitFormBtn } from './SubmitFormBtn'
+import * as Yup from 'yup'
+import { updatePassword } from '../slices/user/userApi'
+import { useDispatch } from 'react-redux'
 
 export const ChangePassword = () => {
-    return <Formik>
+    const dispatch = useDispatch()
+
+    const initialValues = {
+        oldPassword: "",
+        newPassword: ""
+    }
+    
+    const validationSchema = Yup.object({
+        oldPassword: Yup.string().required(),
+        newPassword: Yup.string().required()
+    })
+    
+    const submit = (data, {resetForm}) => {
+        dispatch(updatePassword(data))
+        resetForm()
+    }
+    return <Formik 
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={submit}>
         <Form>
             <Input
                 labelText="Old password"
                 id="password"
                 type="password"
-                fieldName="password"
+                fieldName="oldPassword"
                 placeholder="Enter your old password"
             />
             <Input
                 labelText="New password"
                 id="newpassword"
                 type="password"
-                fieldName="newpassword"
+                fieldName="newPassword"
                 placeholder="Enter your new password"
             />
             <div className='flex justify-end'>
