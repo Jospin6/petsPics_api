d95cc970469184535fefcf675f0e3a1a474113e3
Jospin6 <jospinndagano1@gmail.com>
2024-06-04 16:52:47 +0200

    Change the heaight size to auto

--- client/src/components/CreatePet.jsx
+++ client/src/components/CreatePet.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom'
 export const CreatePet = () => {
 
     return <div className="text-black">
-        <div className="w-full h-[320px] rounded-lg bg-white mt-4 p-2">
+        <div className="w-full h-[auto] rounded-lg bg-white mt-4 p-2">
             <div className="text-lg arial semi-bold">Add new pet</div>
             <NewPetForm/>
         </div>
