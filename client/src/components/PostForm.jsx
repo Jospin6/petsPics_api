918c768631ffe00e737d966177d9be39e0a301f2
Jospin6 <jospinndagano1@gmail.com>
2024-06-06 20:41:42 +0200

    Change the form inputs by using useFormik hook

--- client/src/components/PostForm.jsx
+++ client/src/components/PostForm.jsx
@@ -1,4 +1,4 @@
-import { Form, Formik } from "formik"
+import { useFormik } from "formik"
 import { Input } from "./Input"
 import { Select } from "./Select"
 import { SubmitFormBtn } from './SubmitFormBtn'
@@ -9,6 +9,7 @@ import { getUserPets } from '../helpers/petHelper'
 import * as Yup from 'yup'
 import { useNavigate } from "react-router-dom"
 import { createPost } from '../slices/posts/postApi'
+import { InputFile } from "./InputFile"
 
 export const PostForm = () => {
     const getPets = useSelector(getUserPets)
@@ -22,52 +23,66 @@ export const PostForm = () => {
     }
 
     const validationSchema = Yup.object({
-        pet_id: Yup.number().required(),
-        content: Yup.string().required(),
+        pet_id: Yup.number().required('This field is required'),
+        content: Yup.string().required('This field is required'),
         image: Yup.mixed().required('Image is required')
     })
 
-    useEffect(()  => {
+    useEffect(() => {
         dispatch(fetchUserPets())
     }, [dispatch])
 
-    const submit = (data, { resetForm }) => {
-        dispatch(createPost(data))
-        resetForm()
-        navigate("/user")
-    }
+    const formik = useFormik({
+        initialValues,
+        validationSchema,
+        onSubmit: (data, { resetForm }) => {
+            const formData = new FormData()
+            formData.append('pet_id', data.pet_id)
+            formData.append('content', data.content)
+            formData.append('image', data.image)
+            dispatch(createPost(formData))
+            resetForm()
+            navigate("/user")
+        }
+    })
 
-    return <Formik 
-        initialValues={initialValues} 
-        validationSchema={validationSchema}
-        onSubmit={submit}>
-        <Form>
+    return <div>
+        <form onSubmit={formik.handleSubmit} encType="multipart/form-data">
             <Select
-                labelText="Choose a pet"
                 fieldName="pet_id"
-                id="pet"> 
-                <option value="">Select a pet</option>
+                id="pet"
+                labelText="Choose a pet"
+                onchange={formik.handleChange}
+                value={formik.values.species}>
                 {
                     getPets.map(pet => (
                         <option value={pet.id} key={pet.id}> {pet.petName} </option>
                     ))
                 }
             </Select>
+            {formik.errors.pet_id && formik.touched.pet_id && (
+                <div className="text-red-500">{formik.errors.pet_id}</div>
+            )}
             <Input
-                labelText="Description"
-                id="description"
+                id="content"
                 fieldName="content"
                 type="text"
-                placeholder="Write text here" />
-            <Input
-                labelText="Pet image"
-                id="image"
-                fieldName="image"
-                type="file"
-                placeholder="" />
+                labelText="Description"
+                placeholder="Write text here"
+                onchange={formik.handleChange}
+                value={formik.values.petName} />
+            {formik.errors.content && formik.touched.content && (
+                <div className="text-red-500">{formik.errors.content}</div>
+            )}
+            <InputFile
+                labelText="Upload Image"
+                onchange={(e) => formik.setFieldValue('image', e.target.files[0])} />
+            {formik.errors.image && formik.touched.image && (
+                <div className="text-red-500">{formik.errors.image}</div>
+            )}
             <div className="flex justify-end">
                 <SubmitFormBtn text="Submit" className="w-[150px]" />
             </div>
-        </Form>
-    </Formik>
+        </form>
+    </div>
 }
