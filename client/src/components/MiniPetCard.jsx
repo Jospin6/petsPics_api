2c0ed863b955d1edcc1b9d625af56ecb94edceb1
Jospin6 <jospinndagano1@gmail.com>
2024-07-02 09:40:49 +0200

    Design buttons to delete and edit a pet

--- client/src/components/MiniPetCard.jsx
+++ client/src/components/MiniPetCard.jsx
@@ -2,11 +2,15 @@ import { Image } from "./Image"
 import { getYearFormat } from "../helpers/petHelper"
 export const MiniPetCard = ({ pet }) => {
     return <div className="w-[150px] h-[200px] mt-[5px] mr-2 bg-white rounded-lg small-shadow">
-        <div className="w-full h-[150px] bg-red-300 rounded-t-lg">
+        <div className="w-full h-[150px] bg-red-300 rounded-t-lg relative">
             <Image
                 src={`http://localhost:3001/public/images/${pet.image}`}
                 alt="pet image"
                 style="w-[100%] rounded-t-lg  h-[150px]" />
+            <div className="flex justify-between w-full absolute top-0 letf-0">
+                <button className="bg-red-500 text-white text-[12px] p-[5px] opacity-3 rounded-full">del</button>
+                <button className="bg-blue-500 text-white text-[12px] p-[5px] rounded-full">edit</button>
+            </div>
         </div>
         <div className="pl-2">
             <div>{pet.petName}</div>
