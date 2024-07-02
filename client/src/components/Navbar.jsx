b19acab79c0cbe16bac2adaf92fc0632ff4f0b38
Jospin6 <jospinndagano1@gmail.com>
2024-07-02 12:02:36 +0200

    Add media query to the navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -21,20 +21,20 @@ export const Navbar = () => {
                 petsPics
             </Link>
         </div>
-        <div className="flex items-center justify-between pr-[20px] w-[25%]">
-            <div className='flex'>
+        <div className="flex items-center md:justify-between justify-end w-[50%] md:w-[30%] pr-[20px] w-[25%]">
+            <div className='flex mr-6 md:mr-0'>
                 <Link to="/user/newPet" className='mr-6'>
-                    <Image src={add} alt="Add pet icon" style="w-[25px] h-[25px]"/>
+                    <Image src={add} alt="Add pet icon" style="md:w-[25px] md:h-[25px] w-[18px] h-[18px]"/>
                 </Link>
                 <Link to="/user/likes" className='mr-6'>
-                    <Image src={hearts} alt="Likes" style="w-[25px] h-[25px]"/>
+                    <Image src={hearts} alt="Likes" style="md:w-[25px] md:h-[25px] w-[18px] h-[18px]"/>
                 </Link>
                 <Link to="/user/account" className='mr-2'>
-                    <Image src={male_user} alt="User profil" style="w-[25px] h-[25px]"/>
+                    <Image src={male_user} alt="User profil" style="md:w-[25px] md:h-[25px] w-[18px] h-[18px]"/>
                 </Link>
             </div>
             <button onClick={logout}>
-                <Image src={exit} alt="Logout" style="w-[25px] h-[25px]"/>
+                <Image src={exit} alt="Logout" style="md:w-[25px] md:h-[25px] w-[18px] h-[18px]"/>
             </button>
         </div>
     </div>
